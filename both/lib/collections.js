b875383a976e714d254ab2e5e2f88b5f24f68ef2
Joris Bontje <jorisbontje@gmail.com>
2017-01-21 14:06:51 +0100

    Meteor update and JSON endpoints

--- both/lib/collections.js
+++ both/lib/collections.js
@@ -9,5 +9,13 @@ if (Meteor.isServer) {
   Meteor.publish('dapps', function () {
     // TODO limit this to infinite scroll data
     return App.cols.Dapps.find({})
+  }, {
+    url: "/api/dapps"
+  })
+
+  Meteor.publish('dapps', function (tag) {
+    return App.cols.Dapps.find({tags: tag})
+  }, {
+    url: "/api/dapps-by-tag/:0"
   })
 }
