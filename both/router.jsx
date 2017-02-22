1d56857c8964cf66a8072705fdbd973d06e6cab0
Jr Reyes <jr.evilboss@gmail.com>
2017-02-23 00:31:45 +0800

    [#26] Add dapp route

--- both/router.jsx
+++ both/router.jsx
@@ -4,7 +4,12 @@ FlowRouter.route('/', {
   action () {
     ReactLayout.render(App.DappsList, {})
   }
-})
+});
+FlowRouter.route('dapp/:id', {
+  action (params) {
+    console.log(params.id);
+  }
+});
 
 if (Meteor.isServer) {
   FlowRouter.setDeferScriptLoading(true)
