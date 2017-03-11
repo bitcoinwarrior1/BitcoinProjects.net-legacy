4bc18702b79e717c7a62a6c8fdbfd84733cc2aea
Jr Reyes <jr.evilboss@gmail.com>
2017-03-11 18:41:47 +0800

    refactored props to dapp

--- client/modules/dapps/components/details.jsx
+++ client/modules/dapps/components/details.jsx
@@ -5,45 +5,48 @@ class Details extends React.Component {
   }
 
   render() {
-    const props = this.props
+    const {dapp} = this.props;
     return (
       <div id="details" className="container">
         <div className="row">
           <div className="col s8">
             <div className=" bg-white">
-              <div class="title">
-                <h2>About {props.name}</h2>
+              <div className="title">
+                <h2>About {dapp.name}</h2>
               </div>
               <p>
-                {props.details}
+                {dapp.details}
               </p>
 
-              <a href={props.website} className="btn black">
-                {props.name}
+              <a href={dapp.website} className="btn black">
+                {dapp.name}
               </a>
             </div>
           </div>
           <div className="col s4">
             <div className=" bg-white">
-              <div class="title">
+              <div className="title">
                 <h2>Details</h2>
                 <table>
+                  <tbody>
                   <tr>
                     <td>
                       <b>Name</b>
                     </td>
-                    <td>{props.name}</td>
+                    <td>{dapp.name}</td>
                   </tr>
                   <tr>
                     <td>
                       <b>Website</b>
                     </td>
                     <td>
-                      <a target="_blank" href={props.website}>
-                        {props.website}
+                      <a target="_blank" href={dapp.website}>
+                        {dapp.website}
                       </a>
                     </td>
                   </tr>
+                  </tbody>
+
                 </table>
               </div>
             </div>
@@ -55,15 +58,18 @@ class Details extends React.Component {
 }
 
 Details.defaultProps = {
-  name: 'dapps',
-  website: 'https://dapps.ethercasts.com/',
-  details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' +
-  ' Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, ' +
-  'when an unknown printer took a galley of type and scrambled it to make a type specimen book.' +
-  ' It has survived not only five centuries, but also the leap into electronic typesetting, ' +
-  'remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset ' +
-  'sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus ' +
-  'PageMaker including versions of Lorem Ipsum.',
+  dapp: {
+    name: 'dapps',
+    website: 'https://dapps.ethercasts.com/',
+    details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' +
+    ' Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, ' +
+    'when an unknown printer took a galley of type and scrambled it to make a type specimen book.' +
+    ' It has survived not only five centuries, but also the leap into electronic typesetting, ' +
+    'remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset ' +
+    'sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus ' +
+    'PageMaker including versions of Lorem Ipsum.',
+  }
+
 }
 
 export default Details;
