5a52f9ee8d3c5e98b317ecf1e50c979fb00aabd1
Jr Reyes <jr.evilboss@gmail.com>
2017-03-13 12:37:20 +0800

    remove admin refference in main

--- client/main.js
+++ client/main.js
@@ -3,7 +3,6 @@ import initContext from './configs/context';
 
 // modules
 import coreModule from './modules/core';
-import adminModule from './modules/admin';
 import dappsModule from './modules/dapps';
 
 // init context
@@ -12,6 +11,5 @@ const context = initContext();
 // create app
 const app = createApp(context);
 app.loadModule(coreModule);
-app.loadModule(adminModule);
 app.loadModule(dappsModule);
 app.init();
