84b887cd4519ce2519eb699773970d363c82d10f
James Sangalli <j.l.sangalli@gmail.com>
2017-08-13 16:47:05 +0200

    reduced project status option codes (removing ones that are not needed) and added bitcoin.org link

--- both/views/SubmitModal.js
+++ both/views/SubmitModal.js
@@ -36,7 +36,11 @@ App.SubmitModal = React.createClass({
           <div className='row slim-row center-align'>
             <h4>Submit your Bitcoin project</h4>
             <p>
-              Email <a href='mailto:bitcoinsetupnz@gmail.com?subject=Bitcoin Project Submission&body=Please include your Project Name, Project Description, Organisation name, Website URL, Github URL, Software License (e.g. MIT) and Status: on hold, Stealth mode, Work in progress, Working Prototype or Live'
+              Email <a href='mailto:bitcoinsetupnz@gmail.com?
+              subject=Bitcoin Project Submission&body=Please include your Project Name,
+               Project Description, Organisation name, Website URL, Github URL,
+                Software License (e.g. MIT) and Status: On Hold, Concept, Stealth mode,
+                 Work in progress, Working Prototype or Live'
               target='_blank'>bitcoinsetupnz@gmail.com</a>
             </p>
               <p>with the following information: </p>
@@ -44,10 +48,11 @@ App.SubmitModal = React.createClass({
               Software License (e.g. MIT) and Status </strong>:
               </p>
               <p>1. On Hold </p>
-              <p>2. Stealth Mode </p>
-              <p>3. Work In Progress </p>
-              <p>4. Working Prototype </p>
-              <p>5. Live </p>
+              <p>2. Concept</p>
+              <p>3. Stealth Mode </p>
+              <p>4. Work In Progress </p>
+              <p>5. Working Prototype </p>
+              <p>6. Live </p>
         </div>
       </div>
     </div>
